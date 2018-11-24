e2a503980726ebc356a7251ce09f39b357cf87ca
ba5eem <baseem@gmail.com>
2018-11-23 14:30:03 -1000

    project update

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
+
+const type = (arg) => {
+  if(arg.constructor === Array){
+    return JSON.stringify(arg);
+  }
+  else if(arg.constructor === Object){
+    let x = JSON.stringify(arg);
+    return x.split(',').join("\n");
+  }
+  else{
+    return arg;
+  }
+}
+
+
+
 const cyan = (arg) => {
-		if(arg === undefined){ console.log('did you mean to console log something') }
-		let bgCyan = '\x1b[46m%s\x1b[0m';
-		console.log(bgCyan, arg);
+	let bgCyan = '\x1b[46m%s\x1b[0m';
+	console.log(bgCyan, type(arg));
+};
+
+const red = (arg) => {
+  let bgRed = '\x1b[41m%s\x1b[0m';
+  console.log(bgRed, type(arg));
+};
+
+const green = (arg) => {
+  let bgGreen = '\x1b[42m%s\x1b[0m';
+  console.log(bgGreen, type(arg));
+};
+
+const yellow = (arg) => {
+  let bgYellow = '\x1b[43m%s\x1b[0m';
+  console.log(bgYellow, type(arg));
+};
+
+const black = (arg) => {
+  let bgBlack = '\x1b[40m%s\x1b[0m';
+  console.log(bgBlack, type(arg));
+};
+
+const magenta = (arg) => {
+  let bgMagenta= '\x1b[45m%s\x1b[0m';
+  console.log(bgMagenta, type(arg));
 };
 
 console.cyan = cyan;
+console.red = red;
+console.green = green;
+console.yellow = yellow;
+console.black = black;
+console.magenta = magenta;
+
 
 module.exports = console;
 
