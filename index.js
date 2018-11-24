d9ac2fdc0aee07fc17e6179e952c331054d53a46
ba5eem <baseem@gmail.com>
2018-11-23 17:47:20 -1000

    project update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const type = (arg) => {
   }
 }
 
+
 var defaultStyle = [
     'background-color: cornflowerblue',
     'font-size: 30px',
@@ -21,7 +22,7 @@ var defaultStyle = [
 
 
 
-const any = (arg, style) => {
+const any = (arg,style) => {
   if(style === undefined){
     style = defaultStyle;
   }
@@ -30,32 +31,34 @@ const any = (arg, style) => {
 
 
 
-const cyan = (arg) => {
+
+
+const cyan = (...arg) => {
   let bgCyan = '\x1b[46m%s\x1b[0m';
   console.log(bgCyan, type(arg));
 };
 
-const red = (arg) => {
+const red = (...arg) => {
   let bgRed = '\x1b[41m%s\x1b[0m';
   console.log(bgRed, type(arg));
 };
 
-const green = (arg) => {
+const green = (...arg) => {
   let bgGreen = '\x1b[42m%s\x1b[0m';
   console.log(bgGreen, type(arg));
 };
 
-const yellow = (arg) => {
+const yellow = (...arg) => {
   let bgYellow = '\x1b[43m%s\x1b[0m';
   console.log(bgYellow, type(arg));
 };
 
-const black = (arg) => {
+const black = (...arg) => {
   let bgBlack = '\x1b[40m%s\x1b[0m';
   console.log(bgBlack, type(arg));
 };
 
-const magenta = (arg) => {
+const magenta = (...arg) => {
   let bgMagenta= '\x1b[45m%s\x1b[0m';
   console.log(bgMagenta, type(arg));
 };
